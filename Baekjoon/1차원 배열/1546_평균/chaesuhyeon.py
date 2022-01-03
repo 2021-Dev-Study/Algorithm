a9f67150df8799ea54e9048151829a46a8923209
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-01-03 19:46:36 +0900

    FEAT : chaesuhyeon 2week

--- Baekjoon/1차원 배열/1546_평균/chaesuhyeon.py
+++ Baekjoon/1차원 배열/1546_평균/chaesuhyeon.py
@@ -3,6 +3,6 @@ a = []
 a = list(map(int, input().split()))
 max_a = max(a)
 score = []
-for i in range(N):
-    score.append(a[i]/max_a * 100)
-print(sum(score)/N) 
+for i in range(N): # 과목의 개수만큼 반복
+    score.append(a[i]/max_a * 100) # 새로운 점수를 구함
+print(sum(score)/N)  # 점수를 과목의 개수로 나눔
--- Baekjoon/1차원 배열/8958_OX퀴즈/chaesuhyeon.py
+++ Baekjoon/1차원 배열/8958_OX퀴즈/chaesuhyeon.py
@@ -1,13 +1,13 @@
-n = int(input())
+n = int(input()) # 테스트 개수 입력
 
 for _ in range(n):
     ox_list = list(input())
     score = 0  
-    sum_score = 0 
+    sum = 0 
     for ox in ox_list:
         if ox == 'O':
-            score += 1 
-            sum_score += score
+            score += 1 # 'O'가 연속했을 때 누적됨
+            sum += score
         else:
-            score = 0
-    print(sum_score)
+            score = 0 # "X"이면 score 초기화 
+    print(sum)
