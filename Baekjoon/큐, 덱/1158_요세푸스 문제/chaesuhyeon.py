1a753ac3694abdd58227c4f5a8ee96d53fc2d0af
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2022-01-17 17:04:22 +0900

    FEAT : algorithm study 4week queue

--- Baekjoon/큐, 덱/1158_요세푸스 문제/chaesuhyeon.py
+++ Baekjoon/큐, 덱/1158_요세푸스 문제/chaesuhyeon.py
@@ -3,22 +3,22 @@
 # 4 5 6 7 1 2 (그다음 6제거)
 # 7 1 2 4 5 이런식으로.. 여기까지 생각했는데 구현하는데서 코드가 계속 막혀서 나랑 아이디어 똑같은 풀이를 참고함  
 # 근데 시간 진짜 오래걸리는 풀이..라서 인덱스 풀이 봄
-from collections import deque
-N , K = map(int, input().split())
-q = deque([])
+# from collections import deque
+# N , K = map(int, input().split())
+# q = deque([])
 
-for i in range(1,N+1):
-    q.append(i)   # 1 ~ 7까지 큐에 추가 
+# for i in range(1,N+1):
+#     q.append(i)   # 1 ~ 7까지 큐에 추가 
 
-print('<', end='')
-while q: # 큐가 존재하는 동안 
-    for i in range(K-1): # 두번째 숫자까지 추가하고 빼준다음에 
-        q.append(q[0])
-        q.popleft() 
-    print(q.popleft() , end='') # 세번째 숫자를 빼고 출력해줌 end를 써서 이어서 출력 
-    if q: # 큐가 존재한다면 ,를 써서 출력 형식 맞춰주기 
-        print(', ', end='')
-print('>')        # 큐가 다 pop돼서 while문이 종료되면 >출력해줌 
+# print('<', end='')
+# while q: # 큐가 존재하는 동안 
+#     for i in range(K-1): # 두번째 숫자까지 추가하고 빼준다음에 
+#         q.append(q[0])
+#         q.popleft() 
+#     print(q.popleft() , end='') # 세번째 숫자를 빼고 출력해줌 end를 써서 이어서 출력 
+#     if q: # 큐가 존재한다면 ,를 써서 출력 형식 맞춰주기 
+#         print(', ', end='')
+# print('>')        # 큐가 다 pop돼서 while문이 종료되면 >출력해줌 
 
 
 #################################
