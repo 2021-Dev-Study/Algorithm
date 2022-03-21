5939c08ee7902c4b15f11a46acc8a4409ff8415e
red-Pen9uin <red.pen9uin@gmail.com>
2022-03-21 19:55:37 +0900

    FEAT(이것이코딩테스트다): Implementation 진행중

---
+++ 이것이 코딩 테스트다/구현/12_기둥과 보 설치/red-Pen9uin.py
@@ -0,0 +1,25 @@
+# 작성자: red-Pen9uin
+# 작성일: 2022-03-21
+# Classification: Implementation algorithm
+# 12_기둥과 보 설치
+
+"""########### for time & memory trace ###########"""
+from statistics import variance
+import sys
+sys.path.append('D:\Dropbox\Github-Repository\TIL\mylib')
+import mytracker
+##################################################
+# import sys
+
+def solution(n, x):
+
+
+if __name__ == "__main__":
+    n = 7
+    x = [2, 2, 3, 1, 2, 2, 8]
+
+    solution(n, x)
+
+##################################################
+mytracker.finish()
+"""########### for time & memory trace ###########"""
